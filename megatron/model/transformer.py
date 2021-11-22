bd7609dc21e97225a7007a787dae428f5b9a224c
Will Feng (DPER) <willfeng@fb.com>
2021-11-21 21:22:48 -0800

    update

--- megatron/model/transformer.py
+++ megatron/model/transformer.py
@@ -505,7 +505,7 @@ def get_bias_dropout_add(training):
     return _bias_dropout_add
 
 
-@torch.jit.script
+# @torch.jit.script
 def bias_dropout_add_fused_train(x: torch.Tensor,
                                  bias: torch.Tensor,
                                  residual: torch.Tensor,
@@ -513,7 +513,7 @@ def bias_dropout_add_fused_train(x: torch.Tensor,
     return bias_dropout_add(x, bias, residual, prob, True)
 
 
-@torch.jit.script
+# @torch.jit.script
 def bias_dropout_add_fused_inference(x: torch.Tensor,
                                      bias: torch.Tensor,
                                      residual: torch.Tensor,
