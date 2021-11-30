0d2732722c3de900c2025da43d1de6516ea98dbe
Will Feng (DPER) <willfeng@fb.com>
2021-11-29 23:11:47 -0800

    update

--- pretrain_vit_dummy.py
+++ pretrain_vit_dummy.py
@@ -1,3 +1,66 @@
+"""
+# On AWS GPU node
+
+echo $CUDA_VISIBLE_DEVICES
+
+export NUM_NODES=1
+
+# ViT-H/16
+VIT_ARGS="\
+        --num-attention-heads 16 \
+        --hidden-size 1280 \
+        --num-layers 32 \
+        --tensor-model-parallel-size 1 \
+        --pipeline-model-parallel-size 1 \
+        `# --num-gpus 8` \
+        --global-batch-size 1024 \
+        `# --data-parallel-size 8` \
+        `# --num-micro-batches 1` \
+        --micro-batch-size 128 \
+        --DDP-impl local \
+        --no-contiguous-buffers-in-local-ddp \
+        `# --activations-checkpoint-method uniform` \
+        `# --distribute-checkpointed-activations` \
+        `# --empty-unused-memory-level 2` \
+    \
+        --train-iters 10 \
+        --lr-decay-iters 320000 \
+        --data-impl mmap \
+        --split 949,50,1 \
+        --lr 0.00015 \
+        --lr-decay-style cosine \
+        --min-lr 1.0e-5 \
+        --weight-decay 1e-2 \
+        --clip-grad 1.0 \
+        --lr-warmup-fraction .01 \
+        --log-interval 1 \
+        --save-interval 10000 \
+        --eval-interval 1000 \
+        --eval-iters 1 \
+        --distributed-backend nccl \
+    \
+        --seq-length 196 \
+        --max-position-embeddings 196 \
+        --fp16"
+
+clear && clear
+
+# If AWS cluster
+# HOME_DIR=/fsx/users/willfeng
+# If FRL DGX cluster
+HOME_DIR=/mnt/home/willfeng
+
+DISTRIBUTED_ARGS="--nproc_per_node 8 \
+                  --nnodes ${NUM_NODES} \
+                  --node_rank 0 \
+                  --master_addr localhost \
+                  --master_port 6100"
+
+python -m torch.distributed.launch $DISTRIBUTED_ARGS \
+${HOME_DIR}/repos/Megatron-LM/pretrain_vit_dummy.py $VIT_ARGS
+
+"""
+
 # coding=utf-8
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
