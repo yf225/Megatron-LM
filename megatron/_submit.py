5df3109e4267735c135c5167bc1020ec8d5537c6
Will Feng (DPER) <willfeng@fb.com>
2022-06-06 11:33:39 -0700

    try new submitit design 2/n

--- megatron/_submit.py
+++ megatron/_submit.py
@@ -4,6 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+import argparse
+
+from submitit.core.submission import process_job
+
 
 def submitit_main_patched() -> None:
     parser = argparse.ArgumentParser(description="Run a job")
