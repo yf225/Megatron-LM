f7217664bd31779773b3f97f1a5b765f72719e5f
Will Feng (DPER) <willfeng@fb.com>
2022-07-21 18:33:45 -0700

    update

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -563,7 +563,7 @@ def _add_training_args(parser):
                        help='Disable bias and dropout fusion.',
                        dest='bias_dropout_fusion')
     group.add_argument('--optimizer', type=str, default='adam',
-                       choices=['adam', 'sgd'],
+                       choices=['adam', 'sgd', 'dummy'],
                        help='Optimizer function')
     group.add_argument('--dataloader-type', type=str, default=None,
                        choices=['single', 'cyclic'],
